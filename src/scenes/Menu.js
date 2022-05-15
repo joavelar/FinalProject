158b678ae78cced8a3ec714e6852b9fb35bbaf5a
Jose <joavelar@ucsc.edu>
2022-05-15 11:19:08 -0700

    starts on the menuscene, gives some instructions.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,13 +9,16 @@ class Menu extends Phaser.Scene {
     }
     create() {
 
-        this.add.text(200, 200, "cryptic crawler play scene1");
+        this.add.text(200/2, 200, "This game is about getting pins into the right spot.");
+        this.add.text(200/2, 230, "To fit into the conversation that you are having.");
+        this.add.text(200/2, 260, "Use (w) to move pins up, use (a),(d) to move between pins.");
+
 
         //define the w key
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
 
         //placeholder skip past menu into playscene
-        this.scene.start("playScene");
+        //this.scene.start("playScene");
     }
     update(){
         //press w to start game
