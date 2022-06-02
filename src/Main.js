14197e3d328e4d5fb7f3230733be9a8c094ff5d9
JJHung999 <jjhung@ucsc.edu>
2022-06-02 13:17:52 -0700

    Updated Index and main for scenes handling

--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 720,
     height: 480,
-    scene: [Menu, Play, Credit]
+    scene: [Menu, Play, Credit, Win, Loss, Complete]
 }
 let game = new Phaser.Game(config);
 
